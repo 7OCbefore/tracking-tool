805e53d7e897502cbcc2fc52563992104b3efc7d
7OCbefore <7OCbefore@gmail.com>
2026-05-12 14:23:24 +0800

    feat: add service worker for offline support

---
+++ docs/tracking-tool/sw.js
@@ -0,0 +1,38 @@
+// Service Worker for offline caching
+var CACHE_NAME = 'tracking-tool-v1';
+var URLS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
