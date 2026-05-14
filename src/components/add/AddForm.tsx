7cc58a831f6ffd559e449fafc580eeca6ea9e5cb
7OCbefore <7OCbefore@gmail.com>
2026-05-14 23:19:34 +0800

    chore: add eslint-disable comment for intentional empty deps

--- src/components/add/AddForm.tsx
+++ src/components/add/AddForm.tsx
@@ -33,6 +33,7 @@ export default function AddForm() {
       setNumber(searchToAdd);
       setSearchToAdd(null);
     }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
